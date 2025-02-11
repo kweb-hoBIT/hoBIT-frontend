c685d178b34cc1b477c3282734e5ddc2ac6afd54
Ji Hwan An <angeljihwan@gmail.com>
2025-02-11 18:23:34 +0900

    chore: map when necessary

--- src/components/chat/SeniorResponse.tsx
+++ src/components/chat/SeniorResponse.tsx
@@ -234,7 +234,7 @@ const SeniorResponse: React.FC<SeniorResponseProps> = ({ seniorFaqId }) => {
                           <p className="text-[18px]">{answer.phone}</p>
                         </div>
                       )}
-                      {answer.map && (
+                      {answer.map.latitude && answer.map.longitude && (
                         <div className="flex flex-row items-center mt-[10px]">
                           <CiMap className="mr-[10px] text-[36px] text-[#686D76] bg-white p-[8px] rounded-full" />
                           <a
