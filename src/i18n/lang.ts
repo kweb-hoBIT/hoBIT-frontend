f4d9ea52617122fcf9ef5656fff25f1126ca150f
hiyseo <seo3167@naver.com>
2025-03-19 15:50:35 +0900

    doc: fix header comment

--- src/i18n/lang.ts
+++ src/i18n/lang.ts
@@ -11,7 +11,7 @@ export const lang = {
     faq: '자주 묻는 질문',
   },
   en: {
-    headerTitle: 'Info College(Undergrad) hoBIT',
+    headerTitle: 'Info Chatbot hoBIT',
     questionLabel: 'Question:',
     answerLabel: 'Answer:',
     managerContact: 'Manager Contact:',
