fe0dac4b68de868ddc37ea22bd7b21fc400448d9
MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>
2025-05-14 16:13:13 +0900

    refactor: FAQResponse에 ChatContainer, Text 적용

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,22 +1,29 @@
 interface ChatContainerProps {
-  type?: 'response' | 'query';
+  type?: 'response' | 'query' | 'faq';
   children: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 }
+
+const containerClasses = {
+  response: 'bg-gray-100',
+  query:
+    'bg-[#750E21] ml-auto w-fit inline-block px-[20px] py-[10px] my-[20px]',
+  faq: 'max-w-[300px] md:max-w-[400px] faq-item border border-gray-300 font-5medium w-fit mt-[10px] rounded-[20px] px-[20px] py-[15px] max-w-[400px] break-words inline-block hover:bg-gray-100 transition-colors mr-[10px] cursor-pointer',
+};
 
 const ChatContainer: React.FC<ChatContainerProps> = ({
   children,
   className,
   type = 'response',
+  onClick,
 }) => {
-  const containerClass =
-    type === 'response'
-      ? 'bg-gray-100'
-      : 'bg-[#750E21] ml-auto w-fit inline-block px-[20px] py-[10px] my-[20px]';
+  const containerClass = containerClasses[type];
 
   return (
     <div
       className={`${containerClass} max-w-[250px] md:max-w-[330px] h-auto mt-[20px] rounded-[20px] flex flex-col items-center p-[20px] ${className}`}
+      onClick={onClick}
     >
       {children}
     </div>
