ec4822136ef03d8671e82256707c5aa1e4c027c2
MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>
2025-05-14 16:06:27 +0900

    refactor: Query 컴포넌트 삭제, ChatContainer의 query variant로 변경

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,4 +1,5 @@
 interface ChatContainerProps {
+  type?: 'response' | 'query';
   children: React.ReactNode;
   className?: string;
 }
@@ -6,10 +7,16 @@ interface ChatContainerProps {
 const ChatContainer: React.FC<ChatContainerProps> = ({
   children,
   className,
+  type = 'response',
 }) => {
+  const containerClass =
+    type === 'response'
+      ? 'bg-gray-100'
+      : 'bg-[#750E21] ml-auto w-fit inline-block px-[20px] py-[10px] my-[20px]';
+
   return (
     <div
-      className={`bg-gray-100 max-w-[250px] md:max-w-[330px] h-auto mt-[20px] rounded-[20px] flex flex-col items-center p-[20px] ${className}`}
+      className={`${containerClass} max-w-[250px] md:max-w-[330px] h-auto mt-[20px] rounded-[20px] flex flex-col items-center p-[20px] ${className}`}
     >
       {children}
     </div>
